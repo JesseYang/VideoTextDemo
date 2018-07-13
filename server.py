4981fa742b037202c460d659a2ec668e0b69ff31
JesseYang <jesse.yang1985@gmail.com>
2018-07-13 15:33:25 +0800

    update

--- server.py
+++ server.py
@@ -23,22 +23,13 @@ import json
 import pickle
 
 class ServerAccept:
-    def __init__(self, result_queue, host='192.168.5.42', port=8117):
+    def __init__(self, result_queue, host='192.168.5.41', port=8117):
         print(os.getpid())
         self.host = host
         self.port= port
         self.bufsize = 1024*100
         self.language_name = "english" ## chinese:0 english:1 korean:2 japanese:3
-        self.collections_frame = []
-        self.collections_text = []
         self.result_queue = result_queue
-        # self.capture_queue = Queue(maxsize=cfg.max_queue_len)
-        # self.result_queue = Queue(maxsize=cfg.max_queue_len)
-
-        # self.detect_thread = DetectThread(self.capture_queue, self.result_queue)
-        # self.detect_thread.start()
-        self.output_path = "output_%s.mp4" % str(uuid.uuid4())
- 
         self.socket_server()
 
     def receive_data(self, conn, addr):
@@ -47,8 +38,6 @@ class ServerAccept:
         try:
             conn.settimeout(120)
             print(os.getpid())
-           
-
             buf = b""
             data_len = -1
             while True:
@@ -56,13 +45,8 @@ class ServerAccept:
                 buf += tem_buf
                 if len(buf) >=4 and data_len == -1:
                     data_len = (buf[0]<<24) + (buf[1]<<16) + (buf[2]<<8) + buf[3]
-                #     print("received data size:", data_len)
-                # print(len(buf), len(tem_buf), data_len)
                 if data_len != -1 and (data_len+5) == len(buf):
                     break
-                # print(len(tem_buf), len(buf), data_len)
-                # print(buf.decode('utf-8'))
-                # pdb.set_trace()
             language_idx = buf[4]
             if language_idx == 0:
                 self.language_name = "chinese"
@@ -75,7 +59,7 @@ class ServerAccept:
 
             save_file.write(buf[5:])
             print("received data size %d , language_name %s" % (data_len, self.language_name))
-           
+            conn.close()
         except socket.timeout:
             print("time output: {0}, has closed".format(addr))
             conn.close()
@@ -96,99 +80,86 @@ class ServerAccept:
         print(os.getpid())
 
     def deal_video(self):
+
         ext.from_video("test.mp4", self.language_name)
         ext.save()
         frames = ext.gui(self.language_name)
-        # self.collections_frame = ext.gui_frames
-        # self.collections_text = ext.gui_preds
 
-    def send_result(self, conn, addr):
-        print(conn)
+    def send_result(self, addr, port):
+        time.sleep(2)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((addr, 6667))
+        except socket.error as msg:
+            print("client connect error")
+            print(msg)
+            sys.exit(1)
+
         while True:
 
             flage, gui_img, pre_txt = self.result_queue.get()
-            # print("prepare send data", flage, gui_img, pre_txt)
+
             if flage == 1:
                 img_encode = cv2.imencode('.jpg', gui_img)[1]
                 img_code = np.array(img_encode)
                 str_encode = img_code.tostring()
                 num=len(str_encode)
-                a=[]
-                struc_2 = "bbbbbbbbbbb%ds" % num
+                struc_2 = "bbbbbbbbbbb%dsbbbbbbbbbb%ds" % (num, len(pre_txt))
 
+                a=[]
                 for i in str(num):
                     a.append(int(i))
                 for i in range(10-len(str(num))):
                     a.append(127)
-                print(a)
-                data2 = struct.pack(struc_2, flage, a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9], str_encode)
+
+                b=[]
+                for i in str(len(pre_txt)):
+                    b.append(int(i))
+
+                for i in range(10-len(b)):
+                    b.append(127)
+
+                print(len(str_encode), len(pre_txt))
+                data2 = struct.pack(struc_2, flage, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], str_encode, b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9] , pre_txt.encode('utf-8'))
+                s.send(data2)
+                print("send over, pid", os.getpid())
+               
             elif flage == 0:
-                continue
                 struc_2 = "bb"
                 data2 = struct.pack(struc_2, flage, gui_img)
+                s.send(data2)
+                print("send over, pid", os.getpid())
+                
             else:
-                continue
                 struc_2 = "b"
                 data2 = struct.pack(struc_2, flage)
+                s.send(data2)
+                print("send over, pid", os.getpid())
+                break
 
-                
-            # tip = ['tips/back_squat/good_tip_1.wav', 'tips/back_squat/good_tip_2.wav']
-            # tem_tip = ""
-            # if len(tip) >= 2:
-            #     tem_tip="-".join(e for e in tip)
-            # elif len(tip) == 1:
-            #     tem_tip = tip[0]
-            # else:
-            #     continue
-            # data2 = struct.pack("%ds" % (len(tip)))
-            # result=pickle.dumps((result))
-            conn.send(data2)
-            print("send over, pid")
+        s.close()
             
-
     def deal_data(self):
-        
-        print("waiting for connection...")
-        conn, addr = self.s.accept()
-        # conn.settimeout(30)
-        print("accept new connection from {0}".format(addr))
-        self.receive_data(conn, addr)
+        while True:
+            print("waiting for connection...")
+            conn, addr = self.s.accept()
+            conn.settimeout(30)
+            print("accept new connection from {0}".format(addr))
+            self.receive_data(conn, addr)
 
-        t=threading.Thread(target=self.deal_video)
-        t.start()
+            t=threading.Thread(target=self.deal_video)
+            t.start()
+           
+            t1=threading.Thread(target=self.send_result, args=(addr))
+            t1.start()
         
-        t1=threading.Thread(target=self.send_result, args=(conn, addr))
-        t1.start()
 
 if __name__ == '__main__':
-    #init model
+  
     result_queue = Queue(maxsize=cfg.max_queue_len)
 
     ext = Extractor(result_queue)
 
     server_accept = ServerAccept(result_queue)
+
     server_accept.deal_data()
-
-
-
-
-     # a="faefafa"
-     # # for i in range(len(a)):
-     # #    a+="b"
-     # # for i in range(10-len(a)):
-     # #    a+="0"
-     # # print(a)
-     # a=[]
-     # num=134245
-     # fromat= "bbbbbbbbbb"
-
-     # # c=''
-     # for i in str(num):
-     #  a.append(int(i))
-     # print(a)
-     # for i in range(10-len(str(num))):
-     #  a.append(3)
-     # print(a)
-     # # print(",".join(e for e in a))
-
-     # data=struct.pack(fromat, a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9])
